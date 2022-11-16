da7795016c366358a7dd21387bbc5f40431a6c0d
holakaka <74349746+holakaka@users.noreply.github.com>
2022-11-16 10:31:52 +0700

    plugin custom status

--- wp-content/plugins/woocommerce-checkout-manager/build/backend/js/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('backbone', 'jquery', 'jquery-serializejson', 'jquery-ui-datepicker', 'wp-util'), 'version' => 'f67e0ed96ee9b0e5e761');
--- wp-content/plugins/woocommerce-checkout-manager/build/frontend/js/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('jquery', 'selectWoo'), 'version' => '482dd2ea7a20556d894b');
